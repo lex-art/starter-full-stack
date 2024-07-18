5617b9c4e87bfde613f4b5abe4790baf22e9158b
alex <ochanax@eaglehubs.com>
2024-07-17 18:56:03 -0600

    feat: Update chart component sizes for better visibility

--- ui/src/components/chartsExamples/Guages.tsx
+++ ui/src/components/chartsExamples/Guages.tsx
@@ -6,8 +6,8 @@ import { Gauge } from '@mui/x-charts/Gauge'
 export default function AppGauges() {
 	return (
 		<Stack direction={{ xs: 'column', md: 'row' }} spacing={{ xs: 1, md: 3 }}>
-			<Gauge width={100} height={100} value={60} />
-			<Gauge width={100} height={100} value={60} startAngle={-90} endAngle={90} />
+			<Gauge width={200} height={200} value={60} />
+			<Gauge width={200} height={200} value={60} startAngle={-90} endAngle={90} />
 		</Stack>
 	)
 }
--- ui/src/components/chartsExamples/SparkLine.tsx
+++ ui/src/components/chartsExamples/SparkLine.tsx
@@ -8,10 +8,10 @@ export default function AppSparkLine() {
 	return (
 		<Stack direction="row" sx={{ width: '100%' }}>
 			<Box sx={{ flexGrow: 1 }}>
-				<SparkLineChart data={[1, 4, 2, 5, 7, 2, 4, 6]} height={100} />
+				<SparkLineChart data={[1, 4, 2, 5, 7, 2, 4, 6]} height={200} />
 			</Box>
 			<Box sx={{ flexGrow: 1 }}>
-				<SparkLineChart plotType="bar" data={[1, 4, 2, 5, 7, 2, 4, 6]} height={100} />
+				<SparkLineChart plotType="bar" data={[1, 4, 2, 5, 7, 2, 4, 6]} height={200} />
 			</Box>
 		</Stack>
 	)
