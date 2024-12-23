0fef805f368fe840f64ac82bd131e6144d45a30f
alex <drummerkoka@gmail.com>
2024-12-23 01:12:49 -0600

    Agregar nuevas páginas de usuario y lista de usuarios; actualizar middleware para manejo de rutas internacionalizadas; implementar componente de tarjeta y mejorar el diseño de entrada.

--- ui-v2/src/components/LayoutManager/MainLayout.tsx
+++ ui-v2/src/components/LayoutManager/MainLayout.tsx
@@ -1,7 +1,11 @@
 import { AppSidebar } from '@/components/sidebar/app-sidebar'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import { locales } from '@/i18n/routing'
+import { headers } from 'next/headers'
 import { HeaderMain } from '../header-main'
 import RootLayout from './BaseLayout'
+
+const excludePaths = ['/auth/*', '/landing']
 
 export default async function MainLayout({
 	children,
@@ -10,17 +14,28 @@ export default async function MainLayout({
 	children: React.ReactNode
 	locale: string
 }) {
+	const header = await headers()
+	const currentPath = header.get('x-url') || ''
+	const isExcludeLayout = RegExp(
+		`^(/(${locales.join('|')}))?(${excludePaths.map((p) => p.replace(/\*/g, '.*')).join('|')})$`,
+		'i'
+	).test(currentPath)
+
 	return (
 		<RootLayout locale={locale}>
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset>
-					<HeaderMain />
-					<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-						{children}
-					</div>
-				</SidebarInset>
-			</SidebarProvider>
+			{isExcludeLayout ? (
+				<main>{children}</main>
+			) : (
+				<SidebarProvider>
+					<AppSidebar />
+					<SidebarInset>
+						<HeaderMain />
+						<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+							{children}
+						</div>
+					</SidebarInset>
+				</SidebarProvider>
+			)}
 		</RootLayout>
 	)
 }
--- ui-v2/src/middleware.ts
+++ ui-v2/src/middleware.ts
@@ -1,7 +1,17 @@
 import createMiddleware from 'next-intl/middleware'
+import { NextRequest } from 'next/server'
 import { routing } from './i18n/routing'
 
-export default createMiddleware(routing)
+const handleI18nRouting = createMiddleware(routing)
+
+export default async function middleware(request: NextRequest) {
+	const requestHeaders = new Headers(request.headers)
+	requestHeaders.set('x-url', request.nextUrl.pathname)
+	const newRequest = new NextRequest(request, {
+		headers: requestHeaders
+	})
+	return handleI18nRouting(newRequest)
+}
 
 export const config = {
 	// Match only internationalized pathnames
