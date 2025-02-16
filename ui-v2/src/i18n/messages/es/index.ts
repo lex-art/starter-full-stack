9bf2750ea2d3424b5c16dcca43aa63dfe2a7c6f3
alex <ochanax@eaglehubs.com>
2025-02-16 11:58:33 -0600

    Agregar soporte para autenticación y gestión de sesiones; incluir nuevos tipos y excepciones, y optimizar respuestas de API.

--- ui-v2/src/i18n/messages/es/index.ts
+++ ui-v2/src/i18n/messages/es/index.ts
@@ -1,15 +1,15 @@
 import common from './common.json'
 import components from './components.json'
-import zod from './zod.json'
 import dataTable from './dataTable.json'
 import elements from './elements.json'
+import zod from './zod.json'
 
-const locale = {
+const es = {
 	zod,
 	common,
 	components,
 	dataTable,
 	elements
 }
-export type Locale = typeof locale
-export default locale
+export type Locale = typeof es
+export default es
